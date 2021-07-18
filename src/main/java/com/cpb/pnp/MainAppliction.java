81ed39d10ae090cb381a1ca8d2b373af390df89f
jenil <jenilshah1632@gmail.com>
2021-07-18 12:37:31 +0530

    Server application to test mutual ssl

---
+++ src/main/java/com/cpb/pnp/MainAppliction.java
@@ -0,0 +1,13 @@
+package com.cpb.pnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAppliction {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MainAppliction.class, args);
+	}
+
+}
